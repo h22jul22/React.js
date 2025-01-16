5883188405d8949166ccb5b49516fd48d5e4c4d6
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-01-16 14:41:43 +0900

    chapter_06 State와 생명주기

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import NotificationList from './chapter_06/NotificationList.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <NotificationList />
+    </StrictMode>
+);
