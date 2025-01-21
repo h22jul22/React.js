7195bd98c9bf12bf5bba9703f26077878d93ea12
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-01-21 16:46:20 +0900

    remove: App.jsx, App.css, assets/react.svg

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
 import NotificationList from './chapter_06/NotificationList.jsx';
 import SignUp from './chapter_11/SignUp.jsx';
 
